f8d3ccf79bff1a844ca2285f3b82fafff0031764
KIL Bismuth <kiralowrey@gmail.com>
2020-11-16 00:09:58 -0800

    added all needed table coloumns

--- src/componets/EmployeeRow.js
+++ src/componets/EmployeeRow.js
@@ -3,7 +3,10 @@ import React from "react";
 function EmployeeRow(props) {
   return (
     <tr>
+      <td>{props.employeeRecord.picture.large}</td>
       <td>{props.employeeRecord.name.first} {props.employeeRecord.name.last}</td>
+      <td>{props.employeeRecord.phone}</td>
+      <td>{props.employeeRecord.dob.date}</td>
     </tr>
   );
 }
